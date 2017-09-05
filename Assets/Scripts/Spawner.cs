2b9a588976d2b0b729b49676de6016031d94b214
sarah niemiec <sroseniemiec@gmail.com>
2017-09-05 18:09:21 -0400

    started block game project

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+	public GameObject fallingBlockPrefab;
+	public float secondsBetweenSpawns = 1;
+	float nextSpawnTime;
+
+	Vector2 screenHalfSizeInWorldUnits;
+	// Use this for initialization
+	void Start () {
+		screenHalfSizeInWorldUnits = new Vector2 (Camera.main.aspect * Camera.main.orthographicSize, Camera.main.orthographicSize);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > nextSpawnTime) {
+			nextSpawnTime = Time.time + secondsBetweenSpawns;
+			Vector2 spawnPosition = new Vector2 (Random.Range (-screenHalfSizeInWorldUnits.x, screenHalfSizeInWorldUnits.x), screenHalfSizeInWorldUnits.y + 0.5f);
+			Instantiate (fallingBlockPrefab, spawnPosition, Quaternion.identity);
+		}
+
+	}
+}
